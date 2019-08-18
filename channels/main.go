8ec9fb3a82613e884bb9dee3b6124f7c92377e63
Shakur Ahmed <shakur.ahmed@sahme-mb15tbr.corp.creditkarma.com>
2019-08-18 14:48:00 -0700

    channel must be the same type

--- channels/main.go
+++ channels/main.go
@@ -1,31 +1,64 @@
 package main
 
-import (
+import(
 	"net/http"
 	"fmt"
 )
 
 func main() {
-	links := []string {
-		"http://google.com",
-		"http://yahoo.com",
-		"http://facebook.com",
-		"https://golang.io",
-		"http://amazon.com",
-		"http://creditkarma.com",
+	links := [] string {
+		"https://google.com",
+		"https://twitter.com",
+		"https://facebook.com",
+		"https://verizon.com",
+		"https://golang.org",
+		"https://creditkarma.com",
+		"http://code.corp.creditkarma.com",
 	}
 
 	for _, link := range links {
-		 checkLink(link)
+		 go checkLink(link)
 	}
 }
 
 func checkLink(link string) {
-	_, err := http.Get(link)
-	if err != nil {
-		fmt.Println(link, "is unhealthy!!")
-		return
-	}
-
-	fmt.Println(link, "is super duper healthy")
+	_, err := http.Get(link) 
+		if err != nil {
+			fmt.Println(link, "server is DOWN!")
+			return
+		}
+		
+		fmt.Println(link, "Server Operational!")
 }
+
+// package main
+
+// import (
+// 	"net/http"
+// 	"fmt"
+// )
+
+// func main() {
+// 	links := []string {
+// 		"http://google.com",
+// 		"http://yahoo.com",
+// 		"http://facebook.com",
+// 		"https://golang.io",
+// 		"http://amazon.com",
+// 		"http://creditkarma.com",
+// 	}
+
+// 	for _, link := range links {
+// 		 checkLink(link)
+// 	}
+// }
+
+// func checkLink(link string) {
+// 	_, err := http.Get(link)
+// 	if err != nil {
+// 		fmt.Println(link, "is unhealthy!!")
+// 		return
+// 	}
+
+// 	fmt.Println(link, "is super duper healthy")
+// }
