ad019fbe7eb21cf1493d0359a391d341b32087c3
Shakur Ahmed <shakur.ahmed@sahme-mb15tbr.corp.creditkarma.com>
2019-08-19 19:50:07 -0700

    more practice on http request respo err handling

---
+++ channels/practice.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	links := [] string {
+		"http://yahoo.com",
+		"http://att.com",
+		"http://espn.com",
+		"http://tnt.com",
+		"http://ccb.com"
+	}
+
+	for i := range links {
+		fmp.Printlin(i)
+		if i != nil {
+			
+		}
+	}
+}
